13ea4c2bc3b2b221152ae2ee6069e75c5e0aa0ca
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-08-22 00:25:42 -0300

    new version

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const IMAGES = 19;
+const IMAGES = 82;
 const BACKGROUNDS = 5;
 
 let actualBackground = "bg_0";
 let actualImg = "0";
+let imageApiUrl = "https://raw.githubusercontent.com/jgss-gabriel-sousa/bibi-image-api/main/img/"
 
 function rand(min, max){
     return Math.floor(Math.random() * max) + min;
 }
 
-function loadImage(){
-    let newImg;
-    do{
-        newImg = rand(0, IMAGES).toString();
-    }while(newImg == actualImg)   
+function newImage(){
+    loadImage();
+    changeBackground();
+}newImage();
 
-    document.querySelector("img").src = "img/"+newImg+".jpg";
-    document.getElementById("downloadImage").href = "img/"+newImg+".jpg";
+function loadImage(value){
+    let newImg;
+
+    if(!value){
+        do{
+            newImg = rand(1, IMAGES).toString();
+        }while(newImg == actualImg)   
+    }
+    else newImg = value.toString();
+
+    document.querySelector("img").src = imageApiUrl+"bibi ("+newImg+").jpg";
+    document.getElementById("downloadImage").href = imageApiUrl+"bibi ("+newImg+").jpg";
     actualImg = newImg;
 }
 
@@ -31,7 +41,22 @@ function changeBackground(){
     actualBackground = newBg;
 }
 
-function newImage(){
-    loadImage();
-    changeBackground();
-}newImage();
+function hoursBetweenDates(first, second){
+    const msBetweenDates = Math.abs(first.getTime() - second.getTime());
+    return msBetweenDates / (60 * 60 * 1000);
+}
+
+
+function todayBibi(){
+    const newImage = true;
+
+    if(localStorage.getItem("lastTodayBibi") != null){
+        ;
+    }
+
+    if(newImage){
+        const timeNow = new Date();
+
+        localStorage.setItem("lastTodayBibi",timeNow);
+    }
+}
